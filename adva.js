f7eee25da0d7c2b640116dec9d6dc583b7408c11
jay28122212 <95669684+jay28122212@users.noreply.github.com>
2021-12-15 17:16:11 +0530

    Add files via upload

---
+++ adva.js
@@ -0,0 +1,60 @@
+//reat oerameter
+
+const { first } = require("rxjs")
+
+const calculateAverage = (thing, ...numbers) => {
+    let sum = 0
+    numbers.forEach((num) => sum += num)
+    const average = sum / numbers.length
+    return `The average ${thing} is ${average}`
+}
+
+console.log(calculateAverage('age', 0, 100, 88, 64))
+
+
+
+const printTeam = (teamName, coach, ...players) => {
+    console.log(`Team: ${teamName}`)
+    console.log(`Coach: ${coach}`)
+    console.log(`Players: ${players.join(', ')}`)
+}
+
+printTeam('jay', 'Mehul', 'vishal', 'aryan', 'Hiren', 'krushi')
+
+//spread syntax
+
+
+const team={
+    team:'Enjoy',
+    coach:'jay',
+    players:['jay', 'Mehul', 'vishal', 'aryan', 'Hiren', 'gff']
+}
+
+    printTeam(team.team,team.coach,...team.players)
+
+    //Destructuring
+
+    const dest={
+        id:'bfjheh',
+        Text:'javascript',
+        complete:false
+        
+    }
+
+    
+    const  {Text,complete,details='djhhhf',...others}=dest
+    console.log(Text)
+    console.log(complete)
+    console.log(details)
+    console.log(others)
+
+
+
+
+    const age=[27,55,52,54,22,11]
+
+    const[firstage,secondage,,,,lastage]=age
+
+    console.log(firstage)
+    console.log(secondage)
+    console.log(lastage)
